67cafe55f33021d11f3bd91709b4d9870d421712
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-01 14:25:12 +0000

    1-1 :recycle: Problem broken down a little further

--- src/day1/part1.py
+++ src/day1/part1.py
@@ -1,2 +1,24 @@
 def find_two_entries_which_sum_to_target_and_multiply(entries, target_sum):
+    if len(entries) == 0:
+        return 0
+
+    target_pair_products = map(product_of_a_pair, filter_pairs_by_sum(
+        find_unique_pairs_from_entries(entries), target_sum))
+
+    if (len(target_pair_products)) > 0:
+        return target_pair_products[0]
+
+
+def find_unique_pairs_from_entries(entries):
+    '''Finds unique pairs from the list of entries'''
+    return []
+
+
+def filter_pairs_by_sum(pairs, target_sum):
+    '''Filters a list of pairs by the target_sum'''
+    return []
+
+
+def product_of_a_pair(pair):
+    '''Returns the product of a pair, e.g. a pair of [3, 4] would return 12'''
     return 0
--- src/day1/test_part1.py
+++ src/day1/test_part1.py
@@ -1,7 +1,21 @@
 import part1
 
 
-def test_find_two_entries_which_sum_to_target_and_multiply():
+def test_find_two_entries_which_sum_to_target_and_multiply_1():
+    expense_report = []
+    target_sum = 0
+    assert part1.find_two_entries_which_sum_to_target_and_multiply(
+        expense_report, target_sum) == 0
+
+
+def test_find_two_entries_which_sum_to_target_and_multiply_2():
+    expense_report = [1, 2]
+    target_sum = 3
+    assert part1.find_two_entries_which_sum_to_target_and_multiply(
+        expense_report, target_sum) == 2
+
+
+def test_find_two_entries_which_sum_to_target_and_multiply_3():
     expense_report = [1721, 979, 366, 299, 675, 1456]
     target_sum = 2020
     assert part1.find_two_entries_which_sum_to_target_and_multiply(
