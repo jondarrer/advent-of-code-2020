a0d72f676272c119c3c8e3ac1815895b0f29f0f8
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-05 11:29:07 +0000

    5-2 :white_check_mark: Tests for day 5 part 2

---
+++ src/day5/d5part2.py
@@ -0,0 +1,13 @@
+import d5part1
+import read_input_file
+
+
+def first_missing_seat_id_from_boarding_passes(boading_passes):
+    '''The missing seat id from a list of boarding passes'''
+    return 0
+
+
+if __name__ == '__main__':
+    lines = read_input_file.read(
+        '/Users/jondarrer/Code/advent-of-code-2020/src/input/day5.txt')
+    print(first_missing_seat_id_from_boarding_passes(lines))
---
+++ src/day5/test_d5part2.py
@@ -0,0 +1,8 @@
+import d5part2
+
+
+def test_first_missing_seat_id_from_boarding_passes():
+    boarding_passes = ['FFFFFFFBRRL', 'FFFFFFFBRLR', 'FFFFFFBFLLL']
+    actual = d5part2.first_missing_seat_id_from_boarding_passes(
+        boarding_passes)
+    assert actual == 15
