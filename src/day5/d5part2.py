2ec3bcc3db9eda5a0194cbd021aa1e678056b324
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-05 12:51:20 +0000

    5-2 :sparkles: Day 5 part 2 finished!

--- src/day5/d5part2.py
+++ src/day5/d5part2.py
@@ -2,9 +2,15 @@ import d5part1
 import read_input_file
 
 
-def first_missing_seat_id_from_boarding_passes(boading_passes):
+def first_missing_seat_id_from_boarding_passes(boarding_passes):
     '''The missing seat id from a list of boarding passes'''
-    return 0
+    seat_ids = list(map(lambda boarding_pass: d5part1.convert_seat_to_seat_id(
+        d5part1.convert_boarding_pass_to_seat(boarding_pass)), boarding_passes))
+    seat_ids.sort()
+    index = 0
+    while seat_ids[index] - seat_ids[index + 1] == -1 and index < len(seat_ids):
+        index += 1
+    return seat_ids[index] + 1
 
 
 if __name__ == '__main__':
--- src/day5/test_d5part2.py
+++ src/day5/test_d5part2.py
@@ -2,7 +2,7 @@ import d5part2
 
 
 def test_first_missing_seat_id_from_boarding_passes():
-    boarding_passes = ['FFFFFFFBRRL', 'FFFFFFFBRLR', 'FFFFFFBFLLL']
+    boarding_passes = ['FFFFFFBRRL', 'FFFFFFBRLR', 'FFFFFBFLLL']
     actual = d5part2.first_missing_seat_id_from_boarding_passes(
         boarding_passes)
     assert actual == 15
